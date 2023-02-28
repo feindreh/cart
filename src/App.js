90dcccd7f053ad5082eccccadd9df462ef0a2821
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-28 23:33:29 +0100

    change route name to id

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         <Route path="/Haeckel" element={<Haeckel/>}/>
         <Route path="/Stoff" element={<Stoff/>}/>
         <Route path="/cart" element={<Cart/>}/>
-        <Route path="/buy/:item" element={<Buy/>}/>
+        <Route path="/buy/:id" element={<Buy/>}/>
       </Routes>
     </div>
   );
