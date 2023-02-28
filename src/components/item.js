67180e1ebfcf0bb1315fa4a1602dd53f603f938b
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-28 23:33:19 +0100

    change route name to id

--- src/components/item.js
+++ src/components/item.js
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 
 function Item(props){
     //Link name logo
-    const {name,logo} = props
+    const {name,logo,id} = props
     return(
-        <Link to={`/buy/${name}`}>
+        <Link to={`/buy/${id}`}>
             <div className = "container">
                 <img src={logo} alt=""></img>
                 <div>{name}</div>
