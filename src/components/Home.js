8650ae85577279ef0c5628903c6173107ce576e0
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-28 17:30:18 +0100

    change layout to card

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom"
+import Card from "./card"
+
+import wolleIMG from "../img/wolle.jpg"
+import hero from "../img/hero.jpg"
 
 function Home(){
     return(
-        <div>
-           <Link to="/Wolle">Wolle</Link>
-           <Link to="/Stoff">Stoff</Link>
-           <Link to="/Strick">Strick</Link>
-           <Link to="/Haeckel">Häckeln</Link>
-        </div>
+        <>
+            <div id="hero">
+                <img src={hero} alt=""></img>
+                <div id="hero-text">Katharinas <br></br>Strick Shop</div>
+            </div>
+            <div id="home">
+                <Card link="/Wolle" name="Wolle" logo={wolleIMG}/>
+                <Card link="/Stoff" name="Stoff" logo={wolleIMG}/>
+                <Card link="/Haeckel" name="Häckel" logo={wolleIMG}/>
+                <Card link="/Strick" name="Strick" logo={wolleIMG}/>
+            </div>
+        </>
     )
 }
 
