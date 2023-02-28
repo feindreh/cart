c5f7501b16f25f69893d66c7e5f733691e72f542
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-28 17:52:21 +0100

    add haeckel

--- src/components/Haeckel.js
+++ src/components/Haeckel.js
@@ -1,7 +1,17 @@
+import Item from "./item"
+
+import hackeln1 from "../img/hackeln1.webp"
+import hackeln2 from "../img/hackeln2.webp"
+import hackeln3 from "../img/hackeln3.webp"
+import hackeln4 from "../img/hackeln4.jpeg"
+
 function Haeckel(){
     return(
-        <div>
-            Haeckel
+        <div className = "item-container">
+            <Item logo = {hackeln1} name = {"Häckeln 1"}/>
+            <Item logo = {hackeln2} name = {"Häckeln 2"}/>
+            <Item logo = {hackeln3} name = {"Häckeln 3"}/>
+            <Item logo = {hackeln4} name = {"Häckeln 4"}/>
         </div>
     )
 }
