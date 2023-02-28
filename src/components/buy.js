60d034524c3b7540868bf8e39819df076f626a6c
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-28 23:33:38 +0100

    add component buy

--- src/components/buy.js
+++ src/components/buy.js
@@ -1,11 +1,33 @@
 import { useParams } from "react-router-dom"
+import warehouse from "../warehouse"
 
 function Buy(){
-    const {item} = useParams()
+    const {id} = useParams()
+
+    const item = warehouse.filter(ware => ware.id === +id)[0]
+    const {logo,name,price,unit,description} = item
+    console.log(item)
+    console.log(logo,name,price,unit,description)
     return(
-        <>
-        Buy{item}
-        </>
+        <div className="buy">
+        <div class="imgWrap">
+            <img className = "buyIMG" src={logo}></img>
+        </div>
+        <div class="buyWrap">
+            <div class="buyText">
+                <div class="buyName">
+                    {name}
+                </div>
+                <div class="buyDesc">
+                      {description}
+                </div>
+                <div class="buyPrice">
+                    Preis: {price}€ pro {unit}
+                </div>
+            </div>
+            <button className="buyButton" type="button">Bestellen</button>
+        </div>
+        </div>
     )
 }
 
