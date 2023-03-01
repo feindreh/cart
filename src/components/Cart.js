dc7bc9baf1271ff66c34067cd221dab9372e4434
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-01 02:08:13 +0100

    add cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,36 @@
-function Cart(){
+import { useState } from "react"
+import warehouse from "../warehouse"
+
+function Cart(props){
+
+    const {cart} = props
+    const cartArr = []
+    for(let key in cart){
+        if(cart[key].count !== 0){
+           cartArr.push(cart[key]) 
+        }
+    }
+
+    const sumIt = () => {
+        let sum = 0
+        for(let i = 0; i < cartArr.length;i++){
+            sum += (cartArr[i].count * cartArr[i].price)
+        }
+        return sum
+    }
+    const Sum = sumIt()
+ 
+
+    if(cartArr.length ===0){return <></>}
+    let id = 0
     return(
         <div>
-            Cart
+            {cartArr.map((ware)=>{id++;return <div key={id}>{ware.name} {ware.price.toFixed(2)} € x {ware.count} = {(ware.price * ware.count).toFixed(2)} €</div>})}
+            <div>Summe : {Sum} €</div>
         </div>
     )
 }
 
 export default Cart
+
+
