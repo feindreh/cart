4bc57ddff4ee7f58fae7caf677377cbd193b9f19
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-01 01:27:20 +0100

    add function add from props to button

--- src/components/buy.js
+++ src/components/buy.js
@@ -1,31 +1,40 @@
 import { useParams } from "react-router-dom"
 import warehouse from "../warehouse"
+import Cart from "./Cart"
 
-function Buy(){
+function Buy(props){
+    const {add} = props
     const {id} = useParams()
+
 
     const item = warehouse.filter(ware => ware.id === +id)[0]
     const {logo,name,price,unit,description} = item
 
+
+
+
     return(
         <div className="buy">
-        <div className="imgWrap">
-            <img className = "buyIMG" src={logo}></img>
-        </div>
-        <div className="buyWrap">
-            <div className="buyText">
-                <div className="buyName">
-                    {name}
+            <div className="buyWrap">
+                <div className="imgWrap">
+                    <img className = "buyIMG" src={logo}></img>
                 </div>
-                <div className="buyDesc">
-                      {description}
-                </div>
-                <div className="buyPrice">
-                    Preis: {price}€ pro {unit}
+                <div className="buyText">
+                    <div className="buyName">
+                        {name}
+                    </div>
+                    <div className="buyDesc">
+                        {description}
+                    </div>
+                    <div className="buyPrice">
+                        Preis: {price.toFixed(2)} € pro {unit}
+                    </div>
                 </div>
             </div>
-            <button className="buyButton" type="button">Bestellen</button>
-        </div>
+            <div className="shopit">
+                <button type="button" onClick={()=>[add(id)]}>In den Einkaufswagen</button>
+            </div>
+            <div className="smallCart"><Cart/></div>
         </div>
     )
 }
